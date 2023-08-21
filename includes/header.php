ea3b53cfb3fcbf5bbfe1f03ea6117c6a6fd36b1d
breezelback <breezelback@gmail.com>
2023-08-21 22:40:57 +0800

    Changes

--- includes/header.php
+++ includes/header.php
@@ -377,46 +377,24 @@
                         <a class="product-item_remove" href="javascript:void(0)"><i
                             class="ion-android-close"></i></a>
                         <div class="product-item_img">
-                            <img src="assets/images/product/small-size/1.jpg" alt="Uren's Product Image">
+                            <img src="images/wrench.jpg" alt="Uren's Product Image">
                         </div>
                         <div class="product-item_content">
-                            <a class="product-item_title" href="shop-left-sidebar.html">Autem ipsa ad</a>
-                            <span class="product-item_quantity">1 x $145.80</span>
-                        </div>
-                    </li>
-                    <li class="minicart-product">
-                        <a class="product-item_remove" href="javascript:void(0)"><i
-                            class="ion-android-close"></i></a>
-                        <div class="product-item_img">
-                            <img src="assets/images/product/small-size/2.jpg" alt="Uren's Product Image">
-                        </div>
-                        <div class="product-item_content">
-                            <a class="product-item_title" href="shop-left-sidebar.html">Tenetur illum amet</a>
-                            <span class="product-item_quantity">1 x $150.80</span>
-                        </div>
-                    </li>
-                    <li class="minicart-product">
-                        <a class="product-item_remove" href="javascript:void(0)"><i
-                            class="ion-android-close"></i></a>
-                        <div class="product-item_img">
-                            <img src="assets/images/product/small-size/3.jpg" alt="Uren's Product Image">
-                        </div>
-                        <div class="product-item_content">
-                            <a class="product-item_title" href="shop-left-sidebar.html">Non doloremque placeat</a>
-                            <span class="product-item_quantity">1 x $165.80</span>
+                            <a class="product-item_title" href="shop.php">Wrench</a>
+                            <span class="product-item_quantity">1 x P200.00</span>
                         </div>
                     </li>
                 </ul>
             </div>
             <div class="minicart-item_total">
                 <span>Subtotal</span>
-                <span class="ammount">$462.4‬0</span>
+                <span class="ammount">P200.00</span>
             </div>
             <div class="minicart-btn_area">
-                <a href="cart.html" class="uren-btn uren-btn_dark uren-btn_fullwidth">Minicart</a>
+                <a href="cart.php" class="uren-btn uren-btn_dark uren-btn_fullwidth">Minicart</a>
             </div>
             <div class="minicart-btn_area">
-                <a href="checkout.html" class="uren-btn uren-btn_dark uren-btn_fullwidth">Checkout</a>
+                <a href="checkout.php" class="uren-btn uren-btn_dark uren-btn_fullwidth">Checkout</a>
             </div>
         </div>
     </div>
